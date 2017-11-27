e30c633c128201f9c6302052abeb646b16687640
medokadem <kademmedo@gmail.com>
2017-11-27 21:11:28 +0200

    Revert "struct - identifier"
    This reverts commit e44c93b27e4dc02ec438e6ac65e2703b8581613b.

--- src/compiler/sample inputs/example1.cs
+++ src/compiler/sample inputs/example1.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication2
         {
             
         }
-        public class 12
+        public clas 12
         {
          void ff(){
           for[int i=0;i<n;i+=1]
--- src/compiler/visually output/js/info.js
+++ src/compiler/visually output/js/info.js
@@ -1,8 +1,9 @@
-var input = 'example1.cs';var compile_start = '3:6:50:642:50:200'; var lex_end = '3:6:50:643:51:900';var lex_elapsed = '0.001002';
-var parse_end = '3:6:50:658:65:400';var parse_elapsed = '0.016015';
-var error_recoverd = true;
-var error_num = 3;
+var input = 'example1.cs';var compile_start = '2:57:18:471:96:700'; var lex_end = '2:57:18:471:96:700';var lex_elapsed = '0.000000';
+var parse_end = '2:57:18:488:112:100';var parse_elapsed = '0.018015';
+var error_recoverd = false;
+var error_num = 4;
 var errors = [];
-errors.push({ line: 9, col : 22, msg: 'syntax error, unexpected int_value, expecting IDENTIFIER'});
+errors.push({ line: 9, col : 21, msg: 'syntax error, unexpected int_value, expecting IDENTIFIER or OPERATOR or THIS or STAR'});
 errors.push({ line: 12, col : 14, msg: 'syntax error, unexpected [, expecting ('});
 errors.push({ line: 12, col : 31, msg: 'syntax error, unexpected ], expecting )'});
+errors.push({ line: 19, col : 1, msg: 'syntax error, unexpected }'});
