7986584992d693526178c49ebb7d66bb8af538da
maher alkassir <maheralkassir@hotmail.com>
2017-12-26 02:41:24 +0200

    fix

--- src/compiler/sample inputs/input/example1.cs
+++ src/compiler/sample inputs/input/example1.cs
@@ -1,34 +1,6 @@
-<<<<<<< HEAD
-using x.y.z;
-namespace x
-=======
-class x
->>>>>>> origin/medo-final
-{
-    namespace y
-    {
-        namespace z
-        {
-            class a
-            {
 
-            }
-        }
-    }
-}
-class qq : a
+class qq
 {
 
 }
-class qq3 : a
-{
 
-}
-class qq2 : a
-{
-
-}
-class q3q : a
-{
-
-}
--- src/compiler/sample inputs/input/example2.cs
+++
@@ -1,8 +0,0 @@
-using x.y.z;
-class qq2d : a
-{
-    public static void Main(string[] args)
-    {
-        
-    }
-}
