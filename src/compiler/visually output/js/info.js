e525a32d041dba8925dee450afd235716bf14e27
maher alkassir <maheralkassir@hotmail.com>
2018-04-09 01:36:28 +0300

    damascus fix

--- src/compiler/visually output/js/info.js
+++ src/compiler/visually output/js/info.js
@@ -1,5 +1,5 @@
-var compile_start = '0:19:33:859:694:600'; var lex_end = '0:19:33:861:788:400';var lex_elapsed = '0.002094';
-var compile_start = '0:19:33:859:694:600'; var lex_end = '0:19:33:877:449:900';var lex_elapsed = '0.017755';
+var compile_start = '0:35:7:926:342:800'; var lex_end = '0:35:7:928:186:900';var lex_elapsed = '0.001844';
+var compile_start = '0:35:7:926:342:800'; var lex_end = '0:35:7:948:377:800';var lex_elapsed = '0.022035';
 var error_recoverd = true;
 var error_num = 1;
 var errors = [];
